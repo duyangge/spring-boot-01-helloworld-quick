384392c621a366d85dfe02959d3852d772ec3dee
hxs <2380110794@qq.com>
2020-08-19 20:16:43 +0800

    add huTool plugin

--- src/main/java/cn/jx/pxc/springboot/controller/HelloController.java
+++ src/main/java/cn/jx/pxc/springboot/controller/HelloController.java
@@ -1,20 +1,26 @@
 package cn.jx.pxc.springboot.controller;
 
+import cn.jx.pxc.springboot.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 功能简述:
- *
+ * @RestController相当于@ResponseBody + @Controller
  * @author 23801
  * @version 1.0.0
  * @create 2020/8/9
  * @Date 2020/8/9 20:21
  */
-/**@ResponseBody
-@Controller*/
 @RestController
 public class HelloController {
+
+    @Autowired
+    private HelloService getHelloService;
+
 
     @RequestMapping("/hello")
     public String show(){
